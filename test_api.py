31e1fc2d63e56a3c9595269a85e5a41084832850
Paul Miller <szcezliwy@hotmail.com>
2026-06-18 14:24:22 -0400

    Add character name translation and loop through all characters

--- test_api.py
+++ test_api.py
@@ -17,31 +17,31 @@ STAT_KEYS = {
 "er": "23"
 }
 
-print(data)
-
-character = data['avatarInfoList'][0]
-
-avatar_id =  character['avatarId']
-level = character['propMap']['4001']['ival']
-hp = int(character['fightPropMap'][STAT_KEYS["hp"]])
-atk = int(character['fightPropMap'][STAT_KEYS["atk"]])
-defense = int(character['fightPropMap'][STAT_KEYS["def"]])
-crit_rate = round(character['fightPropMap'][STAT_KEYS["crit_rate"]] * 100, 1)
-crit_dmg = round(character['fightPropMap'][STAT_KEYS["crit_dmg"]] * 100, 1)
-em = int(character['fightPropMap'][STAT_KEYS["em"]])
-er = int(character['fightPropMap'][STAT_KEYS["er"]] * 100)
-
-print(avatar_id, level, hp, atk, defense, crit_rate, crit_dmg, em, er)
+AVATAR_NAMES = {
+    10000042: "Keqing",
+}
 
 def get_db_connection():
     return psycopg2.connect(dbname="genshindb", user="paul", password="7285", host="127.0.0.1",)
 
-def insert_character():
+def insert_character(uid, avatar_id, name, level, hp, atk, defense, em, er, crit_rate, crit_dmg):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO characters (uid, avatar_id, level, hp, atk, def, em, er, crit_rate, crit_dmg)"
-                   "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (uid, avatar_id, level, hp, atk, defense, em, er, crit_rate, crit_dmg,))
+    cursor.execute("INSERT INTO characters (uid, avatar_id, name, level, hp, atk, def, em, er, crit_rate, crit_dmg)"
+                   "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (uid, avatar_id, name, level, hp, atk, defense, em, er, crit_rate, crit_dmg,))
     conn.commit()
     conn.close()
 
-insert_character()
+characters = data['avatarInfoList']
+for character in characters:
+    avatar_id =  character['avatarId']
+    name = AVATAR_NAMES.get(avatar_id, "Unknown")
+    level = character['propMap']['4001']['ival']
+    hp = int(character['fightPropMap'][STAT_KEYS["hp"]])
+    atk = int(character['fightPropMap'][STAT_KEYS["atk"]])
+    defense = int(character['fightPropMap'][STAT_KEYS["def"]])
+    crit_rate = round(character['fightPropMap'][STAT_KEYS["crit_rate"]] * 100, 1)
+    crit_dmg = round(character['fightPropMap'][STAT_KEYS["crit_dmg"]] * 100, 1)
+    em = int(character['fightPropMap'][STAT_KEYS["em"]])
+    er = int(character['fightPropMap'][STAT_KEYS["er"]] * 100)
+    insert_character(uid, avatar_id, name, level, hp, atk, defense, em, er, crit_rate, crit_dmg)
