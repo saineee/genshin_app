d8e2921c045249ace258ead55c86ff7882a4685a
Paul Miller <szcezliwy@hotmail.com>
2026-06-21 19:17:15 -0400

    Add /characters route which returns character data as JSON via SQLAlchemy query

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from models import Character
+from db import Session
+from sqlalchemy import select
 
 app = Flask(__name__)
 
+#create app route to default homepage
 @app.route("/")
 def home():
     return "Hello, from the Genshin Impact Tracker homepage!"
 
+@app.route("/characters")
+def characters():
+    session = Session()
+    characters = session.execute(select(Character)).scalars().all()
+    return {"characters": [character.name for character in characters]}
 
 if __name__ == "__main__":
     app.run(debug=True)
