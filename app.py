fb964402387339887690b5b06c479968ec42560a
Paul Miller <szcezliwy@hotmail.com>
2026-06-21 19:30:48 -0400

    Expand the /character route to return full character details as JSON

--- app.py
+++ app.py
@@ -14,7 +14,13 @@ def home():
 def characters():
     session = Session()
     characters = session.execute(select(Character)).scalars().all()
-    return {"characters": [character.name for character in characters]}
+    return {"characters": [
+        {"name": character.name, "level": character.level, "hp": character.hp, "atk": character.atk,
+         "def": character.def_, "em": character.em, "er": character.er, "crit_rate": character.crit_rate,
+         "crit_dmg": character.crit_dmg, "constellation_lvl": character.constellation_lvl, "weapon_name": character.weapon_name,
+         "talent_na": character.talent_na, "talent_burst": character.talent_burst, "friendship_lvl": character.friendship_lvl}
+        for character in characters]
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
