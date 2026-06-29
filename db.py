b363d01c8678e5ae7ee1ffba487ec92a599fb4a8
Paul Miller <szcezliwy@hotmail.com>
2026-06-28 22:54:54 -0400

    feat: add MIT license

--- db.py
+++ db.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 # load variables from .env into environment
 load_dotenv()
 
-engine = create_engine(
-    f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}")
+engine = create_engine(os.getenv('DATABASE_URL'))
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
