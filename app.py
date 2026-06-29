7bf4d5fa05cde78980d58fc5b55d336b29112dd6
Paul Miller <szcezliwy@hotmail.com>
2026-06-28 20:05:34 -0400

    feat: add homepage with UID form, clean up routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from models import Character
 from db import Session
 from sqlalchemy import select
@@ -9,38 +9,12 @@ from requests.exceptions import Timeout, ConnectionError, HTTPError
 
 app = Flask(__name__)
 
-
-# create app route to default homepage
 @app.route("/")
 def home():
-    return "Hello, from the Genshin Impact Tracker homepage!"
+    return render_template("home.html", error=None)
 
-
-# character route, returns JSON data for each character individually including artifacts
-@app.route("/characters")
+@app.route("/characters", methods=["GET", "POST"])
 def characters():
-    session = Session()
-    characters = session.execute(select(Character)).scalars().all()
-    return {"characters": [
-        {"name": character.name, "level": character.level, "hp": character.hp, "atk": character.atk,
-         "def": character.def_, "em": character.em, "er": character.er, "crit_rate": character.crit_rate,
-         "crit_dmg": character.crit_dmg, "constellation_lvl": character.constellation_lvl,
-         "weapon_name": character.weapon_name,
-         "talent_na": character.talent_na, "talent_burst": character.talent_burst,
-         "talent_skill": character.talent_skill, "friendship_lvl": character.friendship_lvl,
-         "dmg_bonus_type": character.dmg_bonus_type, "dmg_bonus_val": character.dmg_bonus_val,
-         "artifacts": [{"slot": artifact.slot,
-                        "set_name": artifact.set_name, "main_stat": artifact.main_stat,
-                        "main_stat_val": artifact.main_stat_val, "sub1": artifact.sub1,
-                        "sub1_val": artifact.sub1_val, "sub2": artifact.sub2, "sub2_val": artifact.sub2_val,
-                        "sub3": artifact.sub3, "sub3_val": artifact.sub3_val,
-                        "sub4": artifact.sub4, "sub4_val": artifact.sub4_val} for artifact in character.artifacts]} for
-        character in characters]
-    }
-
-
-@app.route("/characters/view", methods=["GET", "POST"])
-def characters_view():
     session = Session()
     if request.method == "POST":
         uid = request.form.get("uid")
@@ -71,8 +45,7 @@ def characters_view():
         return render_template("characters.html", characters=characters, showcase_empty=showcase_empty,
                                player_info=player_info, stygian_difficulty=stygian_difficulty)
     else:
-        characters = session.execute(select(Character)).scalars().all()
-        return render_template("characters.html", characters=characters)
+        return redirect("/")
 
 
 if __name__ == "__main__":
