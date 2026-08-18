96068e32c97229a8dbe3065c6d2231db448f6afe
Paul M <52940016+saineee@users.noreply.github.com>
2026-08-18 18:35:28 -0400

    chore: add terraform aws provider config (#9)

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,4 +7,14 @@ terraform {
     use_lockfile = true
   }
   required_version = ">= 1.10"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+provider "aws" {
+  region              = "us-east-1"
+  allowed_account_ids = ["330866750121"]
 }
