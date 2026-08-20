4507d2a8a4c4d70e8ef7dc23ceaae201dc86ab43
Paul M <52940016+saineee@users.noreply.github.com>
2026-08-20 19:02:58 -0400

    chore: split terraform config into iam and network files (#16)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -58,32 +58,3 @@ resource "aws_iam_role" "ghauth" {
   force_detach_policies = false
   name                  = "GHAuth"
 }
-
-resource "aws_security_group" "genshin_app_sg" {
-  vpc_id = "vpc-014e3c9cf478486ff"
-  name   = "genshin-app-sg"
-  ingress {
-    from_port   = 5000
-    protocol    = "tcp"
-    to_port     = 5000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "genshin_rds_sg" {
-  vpc_id = "vpc-014e3c9cf478486ff"
-  name   = "genshin-rds-sg"
-
-  ingress {
-    from_port       = 5432
-    protocol        = "tcp"
-    to_port         = 5432
-    security_groups = [aws_security_group.genshin_app_sg.id]
-  }
-}
