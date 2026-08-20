26a682090affb5b13221a1d8d4c9d82525f05dce
Paul M <52940016+saineee@users.noreply.github.com>
2026-08-20 08:30:42 -0400

    chore: import ecs cluster and service into terraform (#11)

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,37 @@ import {
 resource "aws_ecr_repository" "genshin_app" {
   name = "genshin-app"
 }
+
+import {
+  to = aws_ecs_cluster.genshin_cluster
+  id = "genshin-cluster"
+}
+
+resource "aws_ecs_cluster" "genshin_cluster" {
+  name = "genshin-cluster"
+}
+
+import {
+  to = aws_ecs_service.genshin_service
+  id = "genshin-cluster/genshin-service"
+}
+resource "aws_ecs_service" "genshin_service" {
+  name    = "genshin-service"
+  cluster = aws_ecs_cluster.genshin_cluster.id
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+  desired_count    = 1
+  launch_type      = "FARGATE"
+  platform_version = "LATEST"
+
+  deployment_configuration {
+    strategy = "ROLLING"
+  }
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = ["sg-028c4c4d1cc2cb8d6"]
+    subnets = ["subnet-0677cf7ea85b8aa54",
+    "subnet-0a9afdc166082a180", "subnet-0c87013b39a033a8c"]
+  }
+}
