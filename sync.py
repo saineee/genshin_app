66a1b06c6fff177a83c1f6ae35adc48830b3f179
Paul M <52940016+saineee@users.noreply.github.com>
2026-08-22 17:19:06 -0400

    fix/session lifecycle (#23)
    * fix: context manager for db sessions
    
    * ci: compileall before install

--- sync.py
+++ sync.py
@@ -13,34 +13,34 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
     
     uid = sys.argv[1]
-    session = Session()
+    with Session() as session:
 
-    player_data = fetch_player_data(uid)
-    for character in player_data['avatarInfoList']:
-        char_data = parse_character(character, uid)
-        try:
-            CharacterSchema(**char_data)
-        except ValidationError as e:
-            log.error(f"Character has wrong schema: {e}")
-            continue
-        character_id = insert_character(session, char_data)
-        if character_id is None:
-            continue
-        all_artifact_data = parse_artifacts(character)
-        for artifact in all_artifact_data:
+        player_data = fetch_player_data(uid)
+        for character in player_data['avatarInfoList']:
+            char_data = parse_character(character, uid)
             try:
-                ArtifactSchema(**artifact)
+                CharacterSchema(**char_data)
             except ValidationError as e:
-                log.error(f"Artifact has wrong schema: {e}")
+                log.error(f"Character has wrong schema: {e}")
                 continue
-            try:
-                insert_artifact(session, artifact, character_id)
-            except IntegrityError as e:
-                session.rollback()
-                log.error(f"Duplicate artifact detected: {e}")
-                raise
-            except Exception as e:
-                session.rollback()
-                log.error(f"Unknown error: {e}")
-                raise
-        session.commit()
+            character_id = insert_character(session, char_data)
+            if character_id is None:
+                continue
+            all_artifact_data = parse_artifacts(character)
+            for artifact in all_artifact_data:
+                try:
+                    ArtifactSchema(**artifact)
+                except ValidationError as e:
+                    log.error(f"Artifact has wrong schema: {e}")
+                    continue
+                try:
+                    insert_artifact(session, artifact, character_id)
+                except IntegrityError as e:
+                    session.rollback()
+                    log.error(f"Duplicate artifact detected: {e}")
+                    raise
+                except Exception as e:
+                    session.rollback()
+                    log.error(f"Unknown error: {e}")
+                    raise
+            session.commit()
